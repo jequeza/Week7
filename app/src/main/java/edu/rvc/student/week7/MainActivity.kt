b5b08bad39ee5eadf79bc3c22090dc8ef0f0acee
Jesus Quezada <maj3st1@gmail.com>
2018-02-22 17:11:39 -0600

    JQ
    Toast app

---
+++ app/src/main/java/edu/rvc/student/week7/MainActivity.kt
@@ -0,0 +1,46 @@
+package edu.rvc.student.week7
+
+import android.content.Context
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.renderscript.ScriptGroup
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //bind objects
+        var txtMessage = findViewById<EditText>(R.id.txtMessage)
+        var btnSubmit = findViewById<Button>(R.id.btnSubmit)
+        //get focus on txtMessage at runtime
+        txtMessage.requestFocus()
+        //Submit Action Button
+        btnSubmit.setOnClickListener{
+            //Showing message by concatenating string and ${variable}
+            //adding Toast message. DO NOT TYPE IN GREYSCALE TEXT context: and text:
+            Toast.makeText(this,"The Message is: ${txtMessage.text.toString()}",Toast.LENGTH_LONG) .show()
+            //Hide keyboard
+            hideKeyboard()
+            //clear txtMessage
+            txtMessage.setText("")
+            //set focus back on txtMessage
+            txtMessage.requestFocus()
+        }
+
+    }
+
+    fun hideKeyboard() {
+        try {
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(currentFocus!!.windowToken,0)
+        } catch (e: Exception) {
+            // TODO: handle exception
+        }
+    }
+
+}
